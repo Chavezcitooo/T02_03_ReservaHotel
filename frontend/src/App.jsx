879ad794f7ff945b7d963146ff08e3378395ab6c
xLeiXF <erlyimoralesl@gmail.com>
2026-07-27 17:41:41 -0500

    Configurar estructura principal y rutas en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
-import HabitacionesPage from "./pages/HabitacionesPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Menu from "./components/Menu.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import HabitacionesPage from "./pages/HabitacionesPage.jsx";
 import "./App.css";
 
 function App() {
-  return <HabitacionesPage />;
+  return (
+    <BrowserRouter>
+      <div className="contenedor">
+        <Header />
+        <Menu />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<Register />} />
+            <Route path="/habitaciones" element={<HabitacionesPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
